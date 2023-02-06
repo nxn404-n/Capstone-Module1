16135157208fd1fc9633325966c3aacffc39f67b
nxn_404 <nafeurrahmanadil@gmail.com>
2023-02-06 21:31:40 +0600

    Add data.js file

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const projectData = [
+  {
+    img: 'img/feature1.jpg',
+    class: 'box10',
+    speakerName: 'Speaker1',
+    p1: 'Lorem ipsum dolor sit amet.',
+    p2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eligendi laborum recusandae iure qui cumque.',
+  },
+  {
+    img: 'img/feature2.jpg',
+    class: 'box11',
+    speakerName: 'Speaker2',
+    p1: 'Lorem ipsum dolor sit amet.',
+    p2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eligendi laborum recusandae iure qui cumque.',
+  },
+  {
+    img: 'img/feature3.jpg',
+    class: 'box12',
+    speakerName: 'Speaker3',
+    p1: 'Lorem ipsum dolor sit amet.',
+    p2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eligendi laborum recusandae iure qui cumque.',
+  },
+  {
+    img: 'img/feature4.jpg',
+    class: 'box13',
+    speakerName: 'Speaker4',
+    p1: 'Lorem ipsum dolor sit amet.',
+    p2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eligendi laborum recusandae iure qui cumque.',
+  },
+  {
+    img: './img/feature5.jpg',
+    class: 'box14',
+    speakerName: 'Speaker5',
+    p1: 'Lorem ipsum dolor sit amet.',
+    p2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eligendi laborum recusandae iure qui cumque.',
+  },
+  {
+    img: 'img/feature6.jpg',
+    class: 'box15',
+    speakerName: 'Speaker6',
+    p1: 'Lorem ipsum dolor sit amet.',
+    p2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eligendi laborum recusandae iure qui cumque.',
+  },
+];
+
+export default projectData;
